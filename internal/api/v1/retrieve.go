8ca0dc73b07efec4c2400cfbbed1138dbb765647
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-17 23:31:09 -0600

    Shorten and retrieve endpoints added

--- internal/api/v1/retrieve.go
+++ internal/api/v1/retrieve.go
@@ -1,21 +1,98 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
+	"github.com/eduardolat/tinylink/internal/echoutil"
 	"github.com/labstack/echo/v4"
 )
 
+type retrieveResponse struct {
+	ShortURL           string     `json:"short_url"`
+	IsActive           bool       `json:"is_active"`
+	Description        *string    `json:"description"`
+	Tags               []string   `json:"tags"`
+	HTTPRedirectCode   int        `json:"http_redirect_code"`
+	OriginalURL        string     `json:"original_url"`
+	ShortCode          string     `json:"short_code"`
+	Clicks             *int64     `json:"clicks"`
+	FirstClickAt       *time.Time `json:"first_click_at"`
+	LastClickAt        *time.Time `json:"last_click_at"`
+	Redirects          *int64     `json:"redirects"`
+	FirstRedirectAt    *time.Time `json:"first_redirect_at"`
+	LastRedirectAt     *time.Time `json:"last_redirect_at"`
+	ExpiresAt          *time.Time `json:"expires_at"`
+	CreatedByIP        *string    `json:"created_by_ip"`
+	CreatedByUserAgent *string    `json:"created_by_user_agent"`
+	CreatedAt          time.Time  `json:"created_at"`
+	UpdatedAt          time.Time  `json:"updated_at"`
+}
+
 func (h *handlers) retrieveHandler(c echo.Context) error {
-	shortCode := c.QueryParam("shortCode")
+	shortCode := c.Param("shortCode")
 	if shortCode == "" {
-		return c.String(http.StatusBadRequest, "shortCode query param is required")
+		return echoutil.JsonError(c, http.StatusBadRequest, errors.New("short code is required"))
 	}
 
 	url, err := h.shortener.RetrieveURL(shortCode)
 	if err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+		return echoutil.JsonError(c, http.StatusInternalServerError, err)
 	}
 
-	return c.String(http.StatusOK, url.OriginalURL)
+	shortUrl := h.shortener.CreateShortURL(url.ShortCode)
+
+	response := retrieveResponse{
+		ShortURL:         shortUrl,
+		IsActive:         url.IsActive,
+		Tags:             url.Tags,
+		HTTPRedirectCode: url.HTTPRedirectCode,
+		OriginalURL:      url.OriginalURL,
+		ShortCode:        url.ShortCode,
+		CreatedAt:        url.CreatedAt,
+		UpdatedAt:        url.UpdatedAt,
+	}
+
+	if url.Description.Valid {
+		response.Description = &url.Description.String
+	}
+
+	if url.Clicks.Valid {
+		response.Clicks = &url.Clicks.Int64
+	}
+
+	if url.FirstClickAt.Valid {
+		response.FirstClickAt = &url.FirstClickAt.Time
+	}
+
+	if url.LastClickAt.Valid {
+		response.LastClickAt = &url.LastClickAt.Time
+	}
+
+	if url.Redirects.Valid {
+		response.Redirects = &url.Redirects.Int64
+	}
+
+	if url.FirstRedirectAt.Valid {
+		response.FirstRedirectAt = &url.FirstRedirectAt.Time
+	}
+
+	if url.LastRedirectAt.Valid {
+		response.LastRedirectAt = &url.LastRedirectAt.Time
+	}
+
+	if url.ExpiresAt.Valid {
+		response.ExpiresAt = &url.ExpiresAt.Time
+	}
+
+	if url.CreatedByIP.Valid {
+		response.CreatedByIP = &url.CreatedByIP.String
+	}
+
+	if url.CreatedByUserAgent.Valid {
+		response.CreatedByUserAgent = &url.CreatedByUserAgent.String
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
--- internal/api/v1/shorten.go
+++ internal/api/v1/shorten.go
@@ -1,24 +1,102 @@
 package v1
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
+	"github.com/eduardolat/tinylink/internal/echoutil"
 	"github.com/eduardolat/tinylink/internal/shortener"
+	"github.com/eduardolat/tinylink/internal/validatorutil"
 	"github.com/labstack/echo/v4"
 )
 
+type shortenRequest struct {
+	ShortCode         string   `json:"short_code"`
+	OriginalURL       string   `json:"original_url" validate:"required,url"`
+	HTTPRedirectCode  int      `json:"http_redirect_code"`
+	Description       string   `json:"description"`
+	Tags              []string `json:"tags"`
+	Password          string   `json:"password"`
+	ExpiresAt         string   `json:"expires_at" validate:"omitempty,datetime"`
+	IsActive          bool     `json:"is_active"`
+	DuplicateIfExists bool     `json:"duplicate_if_exists"`
+}
+
+func NewShortenRequest() *shortenRequest {
+	return &shortenRequest{
+		ShortCode:         "",
+		HTTPRedirectCode:  shortener.HTTPRedirectCodeTemporary,
+		Tags:              []string{},
+		Password:          "",
+		ExpiresAt:         "",
+		IsActive:          true,
+		DuplicateIfExists: false,
+	}
+}
+
+func (s *shortenRequest) BindAndValidate(c echo.Context) error {
+	if err := c.Bind(s); err != nil {
+		return err
+	}
+	return validatorutil.PrettyValidate(s)
+}
+
 func (h *handlers) shortenHandler(c echo.Context) error {
-	url := c.QueryParam("url")
-	if url == "" {
-		return c.String(http.StatusBadRequest, "url query param is required")
+	req := NewShortenRequest()
+	if err := req.BindAndValidate(c); err != nil {
+		return echoutil.JsonError(c, http.StatusBadRequest, err)
+	}
+
+	var parsedExpiresAt time.Time
+	if req.ExpiresAt != "" {
+		var err error
+		parsedExpiresAt, err = time.Parse(time.RFC3339, req.ExpiresAt)
+		if err != nil {
+			return echoutil.JsonError(c, http.StatusBadRequest, err)
+		}
 	}
 
 	shortCode, err := h.shortener.ShortenURL(shortener.StoreURLParams{
-		OriginalURL: url,
+		ShortCode:        req.ShortCode,
+		OriginalURL:      req.OriginalURL,
+		HTTPRedirectCode: req.HTTPRedirectCode,
+		Description: sql.NullString{
+			Valid:  req.Description != "",
+			String: req.Description,
+		},
+		Tags: req.Tags,
+		Password: sql.NullString{
+			Valid:  req.Password != "",
+			String: req.Password,
+		},
+		ExpiresAt: sql.NullTime{
+			Valid: req.ExpiresAt != "",
+			Time:  parsedExpiresAt,
+		},
+		IsActive: req.IsActive,
+		CreatedByIP: sql.NullString{
+			Valid:  true,
+			String: c.RealIP(),
+		},
+		CreatedByUserAgent: sql.NullString{
+			Valid:  true,
+			String: c.Request().UserAgent(),
+		},
+		DuplicateIfExists: req.DuplicateIfExists,
 	})
 	if err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+		return echoutil.JsonError(c, http.StatusInternalServerError, err)
 	}
 
-	return c.String(http.StatusOK, shortCode)
+	shortURL := h.shortener.CreateShortURL(shortCode)
+
+	return c.JSON(
+		http.StatusOK,
+		map[string]string{
+			"short_code": shortCode,
+			"short_url":  shortURL,
+			"created_at": time.Now().Format(time.RFC3339),
+		},
+	)
 }
