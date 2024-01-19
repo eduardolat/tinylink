955a27b33320116ba91e4da89a4223afc9c1b476
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-18 23:34:30 -0600

    Add database queries

---
+++ internal/database/queries/links.sql
@@ -0,0 +1,90 @@
+-- name: Links_ExistsByShortCode :one
+SELECT EXISTS (
+    SELECT 1
+    FROM links
+    WHERE short_code = $1
+) AS exists;
+
+-- name: Links_ExistsByOriginalURL :one
+SELECT EXISTS (
+    SELECT 1
+    FROM links
+    WHERE original_url = $1
+) AS exists;
+
+-- name: Links_GetByShortCode :one
+SELECT * FROM links WHERE short_code = $1;
+
+-- name: Links_GetByOriginalURL :one
+SELECT * FROM links WHERE original_url = $1;
+
+-- name: Links_Create :one
+INSERT INTO links (
+    short_code,
+    original_url,
+    http_redirect_code,
+    is_active,
+    description,
+    tags,
+    password,
+    expires_at,
+    created_by_ip,
+    created_by_user_agent
+) VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9,
+    $10
+) RETURNING *;
+
+-- name: Links_Update :one
+UPDATE links SET
+  short_code = COALESCE($2, short_code),
+  original_url = COALESCE($3, original_url),
+  http_redirect_code = COALESCE($4, http_redirect_code),
+  is_active = COALESCE($5, is_active),
+  description = COALESCE($6, description),
+  tags = COALESCE($7, tags),
+  password = COALESCE($8, password),
+  expires_at = COALESCE($9, expires_at),
+  updated_at = NOW()
+WHERE id = $1 RETURNING *;
+
+-- name: Links_Delete :exec
+DELETE FROM links WHERE id = $1;
+
+-- name: Links_Paginate :many
+SELECT * FROM links
+WHERE (
+  sqlc.narg('filter_is_active')::BOOLEAN IS NULL
+  OR
+  is_active = sqlc.narg('filter_is_active')::BOOLEAN
+)
+AND (
+  sqlc.narg('filter_original_url')::TEXT IS NULL
+  OR
+  original_url ILIKE sqlc.narg('filter_original_url')::TEXT
+)
+AND (
+  sqlc.narg('filter_short_code')::TEXT IS NULL
+  OR
+  short_code ILIKE sqlc.narg('filter_short_code')::TEXT
+)
+AND (
+  sqlc.narg('filter_description')::TEXT IS NULL
+  OR
+  description ILIKE sqlc.narg('filter_description')::TEXT
+)
+AND (
+  ARRAY_LENGTH(sqlc.arg('filter_tags')::TEXT[], 1) = 0
+  OR
+  tags && sqlc.arg('filter_tags')::TEXT[]
+)
+ORDER BY created_at DESC
+LIMIT $1 OFFSET $2;
---
+++ internal/database/queries/visits.sql
@@ -0,0 +1,38 @@
+-- name: Visits_Get :one
+SELECT * FROM visits WHERE id = $1;
+
+-- name: Visits_Create :one
+INSERT INTO visits (
+    link_id,
+    ip,
+    user_agent,
+    referer,
+    is_redirected
+) VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+) RETURNING *;
+
+-- name: Visits_SetIsRedirected :one
+UPDATE visits SET
+  is_redirected = $2
+WHERE id = $1 RETURNING *;
+
+-- name: Visits_PaginateForLink :many
+SELECT * FROM visits
+WHERE link_id = $1
+AND (
+  sqlc.narg('filter_ip')::TEXT IS NULL
+  OR
+  ip ILIKE sqlc.narg('filter_ip')::TEXT
+)
+AND (
+  sqlc.narg('filter_is_redirected')::BOOLEAN IS NULL
+  OR
+  is_redirected = sqlc.narg('filter_is_redirected')::BOOLEAN
+)
+ORDER BY created_at DESC
+LIMIT $2 OFFSET $3;
