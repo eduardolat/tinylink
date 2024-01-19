d156ab6017be10ae88a03605b3c3c92585c99258
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-19 00:34:29 -0600

    Refactor retrieveHandler function and update struct fields

--- internal/api/v1/retrieve.go
+++ internal/api/v1/retrieve.go
@@ -17,12 +17,6 @@ type retrieveResponse struct {
 	HTTPRedirectCode   int        `json:"http_redirect_code"`
 	OriginalURL        string     `json:"original_url"`
 	ShortCode          string     `json:"short_code"`
-	Clicks             *int64     `json:"clicks"`
-	FirstClickAt       *time.Time `json:"first_click_at"`
-	LastClickAt        *time.Time `json:"last_click_at"`
-	Redirects          *int64     `json:"redirects"`
-	FirstRedirectAt    *time.Time `json:"first_redirect_at"`
-	LastRedirectAt     *time.Time `json:"last_redirect_at"`
 	ExpiresAt          *time.Time `json:"expires_at"`
 	CreatedByIP        *string    `json:"created_by_ip"`
 	CreatedByUserAgent *string    `json:"created_by_user_agent"`
@@ -36,7 +30,7 @@ func (h *handlers) retrieveHandler(c echo.Context) error {
 		return echoutil.JsonError(c, http.StatusBadRequest, errors.New("short code is required"))
 	}
 
-	url, err := h.shortener.RetrieveURL(shortCode)
+	url, err := h.shortener.GetByShortCode(shortCode)
 	if err != nil {
 		return echoutil.JsonError(c, http.StatusInternalServerError, err)
 	}
@@ -47,8 +41,8 @@ func (h *handlers) retrieveHandler(c echo.Context) error {
 		ShortURL:         shortUrl,
 		IsActive:         url.IsActive,
 		Tags:             url.Tags,
-		HTTPRedirectCode: url.HTTPRedirectCode,
-		OriginalURL:      url.OriginalURL,
+		HTTPRedirectCode: int(url.HttpRedirectCode),
+		OriginalURL:      url.OriginalUrl,
 		ShortCode:        url.ShortCode,
 		CreatedAt:        url.CreatedAt,
 		UpdatedAt:        url.UpdatedAt,
@@ -58,36 +52,12 @@ func (h *handlers) retrieveHandler(c echo.Context) error {
 		response.Description = &url.Description.String
 	}
 
-	if url.Clicks.Valid {
-		response.Clicks = &url.Clicks.Int64
-	}
-
-	if url.FirstClickAt.Valid {
-		response.FirstClickAt = &url.FirstClickAt.Time
-	}
-
-	if url.LastClickAt.Valid {
-		response.LastClickAt = &url.LastClickAt.Time
-	}
-
-	if url.Redirects.Valid {
-		response.Redirects = &url.Redirects.Int64
-	}
-
-	if url.FirstRedirectAt.Valid {
-		response.FirstRedirectAt = &url.FirstRedirectAt.Time
-	}
-
-	if url.LastRedirectAt.Valid {
-		response.LastRedirectAt = &url.LastRedirectAt.Time
-	}
-
 	if url.ExpiresAt.Valid {
 		response.ExpiresAt = &url.ExpiresAt.Time
 	}
 
-	if url.CreatedByIP.Valid {
-		response.CreatedByIP = &url.CreatedByIP.String
+	if url.CreatedByIp.Valid {
+		response.CreatedByIP = &url.CreatedByIp.String
 	}
 
 	if url.CreatedByUserAgent.Valid {
