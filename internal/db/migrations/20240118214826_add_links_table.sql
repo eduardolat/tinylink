a81a9b24f4d6298966860bc9af08c7c25a33bfbe
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-18 22:58:50 -0600

    Add initial configuration, links table, and visits table migrations

---
+++ internal/db/migrations/20240118214826_add_links_table.sql
@@ -0,0 +1,34 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS links (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  short_code TEXT NOT NULL,
+  original_url TEXT NOT NULL,
+  http_redirect_code INTEGER NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  description TEXT,
+  tags TEXT[],
+  password TEXT,
+  expires_at TIMESTAMP,
+  created_by_ip TEXT,
+  created_by_user_agent TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE links ADD CONSTRAINT links_short_code_unique UNIQUE (short_code);
+CREATE INDEX IF NOT EXISTS links_short_code_idx ON links (short_code);
+CREATE INDEX IF NOT EXISTS links_original_url_idx ON links (original_url);
+CREATE INDEX IF NOT EXISTS links_is_active_idx ON links (is_active);
+CREATE INDEX IF NOT EXISTS links_tags_idx ON links USING GIN (tags);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE links DROP CONSTRAINT IF EXISTS links_short_code_unique;
+DROP INDEX IF EXISTS links_short_code_idx;
+DROP INDEX IF EXISTS links_original_url_idx;
+DROP INDEX IF EXISTS links_is_active_idx;
+DROP INDEX IF EXISTS links_tags_idx;
+DROP TABLE IF EXISTS links;
+-- +goose StatementEnd
---
+++ internal/db/migrations/20240119044917_add_visits_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS visits (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  link_id UUID NOT NULL REFERENCES links (id) ON DELETE CASCADE,
+  ip TEXT NOT NULL,
+  user_agent TEXT NOT NULL,
+  referer TEXT,
+  is_redirected BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS visits_link_id_idx ON visits (link_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS visits_link_id_idx;
+DROP TABLE IF EXISTS visits;
+-- +goose StatementEnd
