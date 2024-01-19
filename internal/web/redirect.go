e426c141710ea5edff5a60be1f25ef3ebd471b74
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-19 15:00:16 -0600

    Add visit tracking and redirection functionality

--- internal/web/redirect.go
+++ internal/web/redirect.go
@@ -1,20 +1,22 @@
 package web
 
 import (
+	"database/sql"
 	"errors"
 	"net/http"
 	"time"
 
+	"github.com/eduardolat/tinylink/internal/database/dbgen"
 	"github.com/eduardolat/tinylink/internal/echoutil"
 	"github.com/eduardolat/tinylink/internal/hashutil"
+	"github.com/eduardolat/tinylink/internal/logger"
 	"github.com/eduardolat/tinylink/internal/shortener"
 	"github.com/eduardolat/tinylink/internal/web/layouts"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/maragudk/gomponents"
 	"github.com/maragudk/gomponents/html"
 )
-
-// TODO: Insert the visits and mark the link as visited
 
 func (h *handlers) redirectHandler(c echo.Context) error {
 	// The short code is the last part of the URL
@@ -23,9 +25,23 @@ func (h *handlers) redirectHandler(c echo.Context) error {
 		return c.Redirect(http.StatusFound, "/404")
 	}
 
-	// Only available in the POST request to check password
-	// protected links
+	// Get the values from the form (only in the POST request)
+	visitIDStr := c.FormValue("visit_id")
 	password := c.FormValue("password")
+
+	// If the visit ID is provided, check if it's valid and use it
+	visitID := uuid.NullUUID{
+		Valid: false,
+	}
+	if visitIDStr != "" {
+		validUuid, err := uuid.Parse(visitIDStr)
+		if err == nil {
+			visitID = uuid.NullUUID{
+				Valid: true,
+				UUID:  validUuid,
+			}
+		}
+	}
 
 	// Retrieve the URL from the shortener service
 	link, err := h.shortener.GetByShortCode(shortCode)
@@ -44,6 +60,31 @@ func (h *handlers) redirectHandler(c echo.Context) error {
 		return c.Redirect(http.StatusFound, "/404")
 	}
 
+	// If we has no visit ID, create a new one
+	if !visitID.Valid {
+		referer := c.Request().Referer()
+		visit, err := h.shortener.CreateVisit(
+			link.ID,
+			dbgen.Visits_CreateParams{
+				LinkID:    link.ID,
+				Ip:        c.RealIP(),
+				UserAgent: c.Request().UserAgent(),
+				Referer: sql.NullString{
+					Valid:  referer != "",
+					String: referer,
+				},
+				IsRedirected: false,
+			},
+		)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		visitID = uuid.NullUUID{
+			Valid: true,
+			UUID:  visit.ID,
+		}
+	}
+
 	// If the link is password protected, check if the password
 	// is provided and correct. Otherwise, show the password
 	// required page.
@@ -57,7 +98,11 @@ func (h *handlers) redirectHandler(c echo.Context) error {
 
 		if !passwordMatch {
 			showPasswordError := passwordProvided
-			page := redirectPasswordPage(shortCode, showPasswordError)
+			page := redirectPasswordPage(
+				shortCode,
+				showPasswordError,
+				visitID,
+			)
 			return echoutil.RenderGomponent(c, http.StatusOK, page)
 		}
 	}
@@ -67,16 +112,41 @@ func (h *handlers) redirectHandler(c echo.Context) error {
 		redirectCode = int(link.HttpRedirectCode)
 	}
 
+	// Mark the visit as redirected
+	if visitID.Valid {
+		_, err = h.shortener.SetVisitAsRedirected(visitID.UUID)
+		if err != nil {
+			logger.Error(
+				"failed to mark visit as redirected",
+				"error",
+				err,
+			)
+		}
+	}
+
 	// Redirect the user to the URL
 	return c.Redirect(int(redirectCode), link.OriginalUrl)
 }
 
-func redirectPasswordPage(shortCode string, showPasswordError bool) gomponents.Node {
+func redirectPasswordPage(
+	shortCode string,
+	showPasswordError bool,
+	visitID uuid.NullUUID,
+) gomponents.Node {
 	return layouts.Public("Password Required", []gomponents.Node{
 		html.H1(gomponents.Text("Password Required")),
 		html.FormEl(
 			html.Method("POST"),
 			html.Action("/"+shortCode),
+
+			gomponents.If(
+				visitID.Valid,
+				html.Input(
+					html.Type("hidden"),
+					html.Name("visit_id"),
+					html.Value(visitID.UUID.String()),
+				),
+			),
 
 			html.Label(
 				html.For("password"),
