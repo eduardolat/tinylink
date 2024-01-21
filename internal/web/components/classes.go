e453dcc415e87146abbd47c12dec263d9dc207e7
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-01-20 21:29:02 -0600

    Add type alias for gomponents.Classes

---
+++ internal/web/components/classes.go
@@ -0,0 +1,6 @@
+package components
+
+import gcomponents "github.com/maragudk/gomponents/components"
+
+// Classes is a type alias for maragudk/gomponents/components.Classes.
+type Classes = gcomponents.Classes
